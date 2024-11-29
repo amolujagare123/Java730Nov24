7404fc0e926138cdf2ca5c6df65c58091cde7d24
amolu <amolujagare@gmail.com>
2024-11-29 19:43:55 +0530

    Initial commit

---
+++ src/Addition.java
@@ -0,0 +1,25 @@
+public class Addition {
+
+    public static void main(String[] args) {
+
+
+        int a = 100 ;
+        int b = 20;
+        int c ;
+
+        c = a + b;
+        System.out.println("Addition=" + c );
+
+        c = a * b;
+        System.out.println("Multiplication=" + c );
+
+        c = a - b;
+        System.out.println("Subtraction=" + c );
+
+        c = a / b;
+        System.out.println("Division=" + c );
+
+
+
+    }
+}
---
+++ src/DataTypeDemo.java
@@ -0,0 +1,44 @@
+public class DataTypeDemo {
+
+    public static void main(String[] args) {
+
+        //-128 to 127
+        byte b = -128 ;
+
+        // -32,768 to 32,767
+        short s = -32768;
+
+        // -2,147,483,648 to 2,147,483,647
+        int i = 2100000000;
+
+        //-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        long l = 4565656568884343434l;
+
+
+        float f = 112.34f;
+        double d = 112.34;
+
+        char ch = '$';
+        // alphabet ( cap /small)
+        // number
+        // special character also
+
+        boolean result = false;
+
+        if(result)
+            System.out.println("My result is true");
+        else
+            System.out.println("My result is false");
+
+
+         int x = 'g' ;
+         System.out.println("x="+x);
+
+         char c1 = 100 ;
+
+        System.out.println("c1="+c1);
+
+
+
+    }
+}
